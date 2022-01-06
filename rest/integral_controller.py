26222157a909f89d6ac422bae6d1177a45212b4a
Maciej Kozłowski <maciekkoz98@gmail.com>
2022-01-06 17:53:30 +0100

    Flat structure, please work

---
+++ rest/integral_controller.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request
+
+from app.service.integral_service import calculate_integral_by_rectangle_rule
+
+integral_blueprint = Blueprint("integral", __name__, url_prefix="/integral")
+
+
+@integral_blueprint.route("")
+def calculate_integral_result():
+    args = request.args
+
+    a = float(args.get("a", 0))
+    b = float(args.get("b", 0))
+    rectangle_number = float(args.get("rectangle_number", 1))
+
+    return str(calculate_integral_by_rectangle_rule(a, b, rectangle_number))
